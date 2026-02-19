a8d0f971dc2fa78cdd70a9ad932b4f49dfeb0eab
varsanydavid20 <varsanyid.24d@acsjszki.hu>
2026-02-19 12:30:09 +0100

    ok

--- nem az/allatok.py
+++ nem az/allatok.py
@@ -1,12 +1,39 @@
-class Allatok:
-    def __init__(self,név,súly):
-        self.név=név
-        self.súly=súly
-     
-Lallatok=[]
+from shlex import split
 
-for i in range(3):
-    név=input("Adja meg az állat nevét: ")
-    súly=int(input("Adja meg az állat súlyát: "))
-    allat=Allatok(név,súly)
-    Lallatok.append(allat)
+
+class magassag:
+    pass
+
+f=open("nem az/diak.txt","r",encoding="utf-8")
+adatok=f.read()
+
+print(adatok)
+
+adatok=adatok.split("\n")
+db=0
+print(f"Az diakok száma: {len(adatok)} fő")
+max_cm = 0
+legmagasabbak = []
+
+for sor in adatok:
+    if sor.strip():
+        nev, cm = sor.strip().split(";")
+        cm = int(cm)
+        if cm > max_cm:
+            max_cm = cm
+            legmagasabbak = [nev]
+        elif cm == max_cm:
+            legmagasabbak.append(nev)
+print(f"Legmagasabb diák: {legmagasabbak[0]} ({max_cm} cm)")
+
+csokkeno_sorrend = []
+for sor in adatok:
+    if sor.strip():
+        nev, cm = sor.strip().split(";")
+        csokkeno_sorrend.append((nev, int(cm)))
+
+csokkeno_sorrend.sort
+
+for nev, cm in csokkeno_sorrend:
+   print(f"{nev}: {cm} cm")
+f.close()
